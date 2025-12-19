e8e427fd41a4a21710ffdd1b4a6c8244db68af6d
bala-amypo <bala@amypo.in>
2025-12-19 05:29:30 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/VolunteerProfile.java
@@ -0,0 +1,60 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "volunteer_profiles")
+public class VolunteerProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String availabilityStatus;
+
+    public VolunteerProfile() {
+    }
+
+    public VolunteerProfile(String name, String email, String availabilityStatus) {
+        this.name = name;
+        this.email = email;
+        this.availabilityStatus = availabilityStatus;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAvailabilityStatus() {
+        return availabilityStatus;
+    }
+
+    public void setAvailabilityStatus(String availabilityStatus) {
+        this.availabilityStatus = availabilityStatus;
+    }
+}
