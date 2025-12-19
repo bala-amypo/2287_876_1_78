92e1f2de334915e423c551ede8d32807344e7158
bala-amypo <bala@amypo.in>
2025-12-19 06:08:30 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,12 +1,15 @@
 package com.example.demo.controller;
 
-
-
-
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.AuthResponse;
+import com.example.demo.dto.RegisterRequest;
 import com.example.demo.model.VolunteerProfile;
-
+import com.example.demo.security.JwtTokenProvider;
 import com.example.demo.service.VolunteerProfileService;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -35,11 +38,14 @@ public class AuthController {
 
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        request.getUsername(), request.getPassword())
+                        request.getUsername(),
+                        request.getPassword())
         );
 
         String token = jwtTokenProvider.generateToken(
-                request.getUsername(), "ROLE_USER");
+                request.getUsername(),
+                "ROLE_USER"
+        );
 
         return new AuthResponse(token, null, "ROLE_USER");
     }
