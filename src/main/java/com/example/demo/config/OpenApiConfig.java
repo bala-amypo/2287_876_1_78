c3912c1758b700e4bf255c9a0a1616493a80eb49
bala-amypo <bala@amypo.in>
2025-12-20 02:33:40 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -4,10 +4,17 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
+
     @Bean
-    public OpenAPI customOpenAPI() { return new OpenAPI().servers(List.of(new Server().url("https://9076.pro604cr.amypo.ai"))); }
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("https://9076.pro604cr.amypo.ai")
+                ));
+    }
 }
