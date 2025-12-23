5cf45bf617f519adcc19585b97ab76a5c3a82f07
bala-amypo <bala@amypo.in>
2025-12-23 14:36:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
+++ src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
@@ -1,12 +1,9 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.AssignmentEvaluationRecord;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface AssignmentEvaluationRecordRepository {
-    AssignmentEvaluationRecord save(AssignmentEvaluationRecord record);
-    Optional<AssignmentEvaluationRecord> findById(Long id);
+    AssignmentEvaluationRecord save(AssignmentEvaluationRecord evaluation);
     List<AssignmentEvaluationRecord> findByAssignmentId(Long assignmentId);
 }
