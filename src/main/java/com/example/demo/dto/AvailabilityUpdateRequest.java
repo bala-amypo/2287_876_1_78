195075a07f90d6bb7b1db717d73a0c4e1e5da639
bala-amypo <bala@amypo.in>
2025-12-24 05:09:58 +0000

    User commit

--- src/main/java/com/example/demo/dto/AvailabilityUpdateRequest.java
+++ src/main/java/com/example/demo/dto/AvailabilityUpdateRequest.java
@@ -1,11 +1,11 @@
-// package com.example.demo.dto;
+package com.example.demo.dto;
 
-// public class AvailabilityUpdateRequest {
-//     private Long volunteerId;
-//     private String availabilityStatus;
+public class AvailabilityUpdateRequest {
+    private Long volunteerId;
+    private String availabilityStatus;
 
-//     public Long getVolunteerId() { return volunteerId; }
-//     public void setVolunteerId(Long volunteerId) { this.volunteerId = volunteerId; }
-//     public String getAvailabilityStatus() { return availabilityStatus; }
-//     public void setAvailabilityStatus(String availabilityStatus) { this.availabilityStatus = availabilityStatus; }
-// }
+    public Long getVolunteerId() { return volunteerId; }
+    public void setVolunteerId(Long volunteerId) { this.volunteerId = volunteerId; }
+    public String getAvailabilityStatus() { return availabilityStatus; }
+    public void setAvailabilityStatus(String availabilityStatus) { this.availabilityStatus = availabilityStatus; }
+}
