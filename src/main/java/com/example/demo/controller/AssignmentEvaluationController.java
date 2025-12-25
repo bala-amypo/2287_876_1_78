24ecc82dc7acd7939c90db81d81531d9118be183
bala-amypo <bala@amypo.in>
2025-12-25 12:27:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/AssignmentEvaluationController.java
+++ src/main/java/com/example/demo/controller/AssignmentEvaluationController.java
@@ -1,29 +1,9 @@
-package com.example.demo.controller;
-
-import com.example.demo.model.AssignmentEvaluationRecord;
-import com.example.demo.service.AssignmentEvaluationService;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/evaluations")
-public class AssignmentEvaluationController {
-
-    private final AssignmentEvaluationService assignmentEvaluationService;
-
-    public AssignmentEvaluationController(AssignmentEvaluationService assignmentEvaluationService) {
-        this.assignmentEvaluationService = assignmentEvaluationService;
-    }
-
-    @PostMapping
-    public AssignmentEvaluationRecord evaluate(@RequestBody AssignmentEvaluationRecord record) {
-        return assignmentEvaluationService.evaluateAssignment(record);
-    }
-
-    @GetMapping("/{assignmentId}")
-    public List<AssignmentEvaluationRecord> getByAssignment(
-            @PathVariable Long assignmentId) {
-        return assignmentEvaluationService.getEvaluationsByAssignment(assignmentId);
-    }
+{
+  "volunteerId": "string",
+  "name": "string",
+  "email": "string",
+  "password": "string",
+  "role": "string",
+  "availability": "string",
+  "skill": "string"
 }
