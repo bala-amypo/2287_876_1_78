0694c681b641a9a2813cac5c00e62500f1e12bed
bala-amypo <bala@amypo.in>
2025-12-25 12:31:08 +0000

    User commit

--- src/main/java/com/example/demo/service/AssignmentEvaluationService.java
+++ src/main/java/com/example/demo/service/AssignmentEvaluationService.java
@@ -6,8 +6,9 @@ import java.util.List;
 public interface AssignmentEvaluationService {
 
     AssignmentEvaluationRecord evaluateAssignment(
-            AssignmentEvaluationRecord evaluation);
+                AssignmentEvaluationRecord evaluation);
 
-    List<AssignmentEvaluationRecord> getEvaluationsByAssignment(
-            Long assignmentId);
-}
+                    List<AssignmentEvaluationRecord> getEvaluationsByAssignment(
+                                Long assignmentId);
+                                }
+                                
