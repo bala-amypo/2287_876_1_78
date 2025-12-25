ae7dfb14baf6a5394a62bf7acafe1557bafc970e
bala-amypo <bala@amypo.in>
2025-12-25 02:27:13 +0000

    User commit

--- src/main/java/com/example/demo/model/AssignmentEvaluationRecord.java
+++ src/main/java/com/example/demo/model/AssignmentEvaluationRecord.java
@@ -1,21 +1,63 @@
 package com.example.demo.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "assignment_evaluations")
 public class AssignmentEvaluationRecord {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long assignmentId; // Missing field for findByAssignment_Id
+    private Long assignmentId; // foreign key
+
     private Integer score;
+
     private String remarks;
+
+    // Default constructor
+    public AssignmentEvaluationRecord() {
+    }
+
+    // Constructor with all fields
+    public AssignmentEvaluationRecord(Long id, Long assignmentId, Integer score, String remarks) {
+        this.id = id;
+        this.assignmentId = assignmentId;
+        this.score = score;
+        this.remarks = remarks;
+    }
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getAssignmentId() {
+        return assignmentId;
+    }
+
+    public void setAssignmentId(Long assignmentId) {
+        this.assignmentId = assignmentId;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
 }
