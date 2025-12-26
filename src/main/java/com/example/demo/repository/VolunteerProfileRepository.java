b28ea84b14e86cb4a80323b0234161afcf273f69
bala-amypo <bala@amypo.in>
2025-12-26 09:30:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/VolunteerProfileRepository.java
+++ src/main/java/com/example/demo/repository/VolunteerProfileRepository.java
@@ -1,20 +1,13 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.VolunteerProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface VolunteerProfileRepository extends JpaRepository<VolunteerProfile, Long> {
+public interface VolunteerProfileRepository
+        extends JpaRepository<VolunteerProfile, Long> {
+
+    Optional<VolunteerProfile> findByVolunteerId(String volunteerId);
 
     boolean existsByVolunteerId(String volunteerId);
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
-
-    Optional<VolunteerProfile> findByVolunteerId(String volunteerId);
 
     List<VolunteerProfile> findByAvailabilityStatus(String status);
 }
