8e633a1f145032e16c7410e8ba59308c5d2f3a50
bala-amypo <bala@amypo.in>
2025-12-27 05:29:57 +0000

    User commit

--- src/main/java/com/example/demo/config/OpenApiConfig.java
+++ src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -22,7 +22,7 @@ public class OpenApiConfig {
                 .bearerFormat("JWT");
 
         Server server = new Server()
-                .url("https://9127.32procr.amypo.ai/")
+                .url("https://9176.32procr.amypo.ai/")
                 .description("Production Server");
 
         return new OpenAPI()
