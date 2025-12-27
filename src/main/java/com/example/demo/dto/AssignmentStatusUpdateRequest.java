573e217a548e9c7a90147b92789435ec3b9cf3ff
bala-amypo <bala@amypo.in>
2025-12-27 13:36:57 +0000

    User commit

--- src/main/java/com/example/demo/dto/AssignmentStatusUpdateRequest.java
+++ src/main/java/com/example/demo/dto/AssignmentStatusUpdateRequest.java
@@ -1,26 +1,19 @@
 package com.example.demo.dto;
 
 public class AssignmentStatusUpdateRequest {
-
     private Long assignmentId;
     private String status;
 
-    public AssignmentStatusUpdateRequest() {
-    }
+    public AssignmentStatusUpdateRequest() {}
 
-    public Long getAssignmentId() {
-        return assignmentId;
-    }
- 
-    public void setAssignmentId(Long assignmentId) {
+    public AssignmentStatusUpdateRequest(Long assignmentId, String status) {
         this.assignmentId = assignmentId;
-    }
- 
-    public String getStatus() {
-        return status;
-    }
- 
-    public void setStatus(String status) {
         this.status = status;
     }
+
+    public Long getAssignmentId() { return assignmentId; }
+    public void setAssignmentId(Long assignmentId) { this.assignmentId = assignmentId; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 }
