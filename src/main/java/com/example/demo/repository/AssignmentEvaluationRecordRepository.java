c3e92bda5febc0f9c8e750a10990f8218354bab3
bala-amypo <bala@amypo.in>
2025-12-27 04:46:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
+++ src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
@@ -4,6 +4,6 @@ import com.example.demo.model.AssignmentEvaluationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface AssignmentEvaluationRepository extends JpaRepository<AssignmentEvaluationRecord, Long> {
+public interface AssignmentEvaluationRecordRepository extends JpaRepository<AssignmentEvaluationRecord, Long> {
     List<AssignmentEvaluationRecord> findByAssignmentId(Long assignmentId);
 }
--- src/main/java/com/example/demo/repository/VolunteerSkillRecordRepository.java
+++ src/main/java/com/example/demo/repository/VolunteerSkillRecordRepository.java
@@ -4,7 +4,7 @@ import com.example.demo.model.VolunteerSkillRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface VolunteerSkillRepository extends JpaRepository<VolunteerSkillRecord, Long> {
+public interface VolunteerSkillRecordRepository extends JpaRepository<VolunteerSkillRecord, Long> {
     List<VolunteerSkillRecord> findByVolunteerId(Long volunteerId);
     List<VolunteerSkillRecord> findBySkillName(String skillName);
     List<VolunteerSkillRecord> findBySkillNameAndSkillLevel(String skillName, String skillLevel);
