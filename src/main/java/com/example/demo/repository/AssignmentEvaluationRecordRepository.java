faadd5e80b75f3d69e73ede941c6c85d70ec6fd4
bala-amypo <bala@amypo.in>
2025-12-27 00:22:16 +0000

    User commit

--- src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
+++ src/main/java/com/example/demo/repository/AssignmentEvaluationRecordRepository.java
@@ -3,10 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.AssignmentEvaluationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface AssignmentEvaluationRepository
+public interface AssignmentEvaluationRecordRepository
         extends JpaRepository<AssignmentEvaluationRecord, Long> {
-
-    List<AssignmentEvaluationRecord> findByAssignmentId(Long assignmentId);
 }
--- src/main/java/com/example/demo/repository/TaskAssignmentRecordRepository.java
+++ src/main/java/com/example/demo/repository/TaskAssignmentRecordRepository.java
@@ -3,10 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.model.TaskAssignmentRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-public interface TaskAssignmentRepository
+public interface TaskAssignmentRecordRepository
         extends JpaRepository<TaskAssignmentRecord, Long> {
-
-    List<TaskAssignmentRecord> findByVolunteerId(Long volunteerId);
 }
+    
