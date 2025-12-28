bd2b998f77b61d419d071975b42b4c6ce821a43f
bala-amypo <bala@amypo.in>
2025-12-28 04:22:27 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.example.demo.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
@@ -26,6 +27,10 @@ public class SwaggerConfig {
                 .description("Production Server");
 
         return new OpenAPI()
+                .info(new Info()
+                        .title("Volunteer Skill Matcher")
+                        .version("1.0")
+                        .description("API for managing volunteer task assignments based on skills"))
                 .addSecurityItem(
                         new SecurityRequirement().addList("bearerAuth")
                 )
